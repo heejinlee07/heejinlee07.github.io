5164cdcd007c73251ee264859e6fdb1cb4dbcd48
heejinlee07 <heejinlee07@gmail.com>
2020-02-27 12:10:57 +0900

    new post

--- source/_drafts/test2.js
+++ source/_drafts/test2.js
@@ -1,11 +1,30 @@
+// function another() {
+//   let x = 1;
+// }
+
+// let x = 5;
+
+// function outer() {
+//   let x = 3;
+//   // 중첩 함수
+//   function inner() {
+//     let y = 2;
+//     // 외부 함수의 변수를 참조할 수 있다.
+//     console.log(x + y); // 3
+//   }
+
+//   return inner;
+// }
+
+// const content = outer();
+// another();
+// content();
+
 function another() {
   let x = 1;
 }
-
-let x = 5;
-
 function outer() {
-  let x = 3;
+  let x = 1;
   // 중첩 함수
   function inner() {
     let y = 2;
